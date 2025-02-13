10e7bbf47c4f2936d410a80700deec534681e6ae
unknown <mikolaj.janczaq@gmail.com>
2025-02-13 13:30:41 +0100

    Add background image to the StartPanel so that it won't be os empyt

--- game/brick_breaker/src/game/StartPanel.java
+++ game/brick_breaker/src/game/StartPanel.java
@@ -1,20 +1,34 @@
 package game;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class StartPanel extends JPanel {
     private GameFrame gameFrame;
+    private Image backgroundImage;
 
     public StartPanel(GameFrame gameFrame) {
         this.gameFrame = gameFrame;
         setLayout(new BorderLayout());
 
-        JLabel titleLabel = new JLabel("Starting Game");
+        try {
+            backgroundImage = ImageIO.read(new File("resources/background.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        JLabel titleLabel = new JLabel("Brick Breaker", SwingConstants.CENTER);
         titleLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
-        titleLabel.setForeground(Color.BLACK);
+        titleLabel.setForeground(Color.WHITE);
+
+        //push the "Brick breaker" down"
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(150, 0, 50, 0));
 
         JButton startButton = new JButton("Start");
         startButton.setFont(new Font("Comic Sans MS", Font.BOLD, 22));
@@ -27,7 +41,17 @@ public class StartPanel extends JPanel {
         add(titleLabel, BorderLayout.CENTER);
         add(startButton, BorderLayout.SOUTH);
         setBackground(Color.BLACK);
+    }
 
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
+        if(backgroundImage != null) {
+            int scaledWidth = getWidth();
+            int scaledHeight = getHeight() / 2;
+            int xOffset = (getWidth() - scaledWidth) / 2;
+
+            g.drawImage(backgroundImage, xOffset, 0, scaledWidth, scaledHeight,null);
+        }
     }
 }
